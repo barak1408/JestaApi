5a141039ac43654ae6c7ddd676228e651a72f421
barak1408 <barakavrani@gmail.com>
2026-06-16 18:01:09 +0300

    commit 61

--- app.js
+++ app.js
@@ -8,6 +8,13 @@ const cors = require('cors');
 
 const uri = process.env.MONGO_STR;
 
+const admin = require("firebase-admin");
+const serviceAccount = require("./serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
 async function connectDB() {
   try {
     await mongoose.connect(uri);
